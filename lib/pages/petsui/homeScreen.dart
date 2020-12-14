8a2e278ae0272c8e6cc0e8244a8ba3afbb1974a5
Aristocrat <crataristo4@gmail.com>
2020-12-14 14:57:11 +0000

    search button added

--- lib/pages/petsui/homeScreen.dart
+++ lib/pages/petsui/homeScreen.dart
@@ -88,6 +88,45 @@ class _PetsHomeScreenState extends State<PetsHomeScreen> {
               )
             ],
           ),
+          SizedBox(
+            height: 24,
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(32),
+                      topLeft: Radius.circular(32)),
+                  color: Colors.grey[300]),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    margin: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Text("Search a pet to adopt"),
+                        IconButton(
+                            icon: Icon(Icons.auto_awesome), onPressed: () {})
+                      ],
+                    ),
+                  ),
+                ],
+              ))
         ],
       ),
     );
