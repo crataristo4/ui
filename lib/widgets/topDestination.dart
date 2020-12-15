72915d2e9f8f24eca52df3cc482edcb73a661c46
Aristocrat <crataristo4@gmail.com>
2020-12-15 19:17:07 +0000

    fixed bugs

--- lib/widgets/topDestination.dart
+++ lib/widgets/topDestination.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ui/models/travelui/destination.dart';
 
 class TopDestination extends StatelessWidget {
@@ -37,22 +38,110 @@ class TopDestination extends StatelessWidget {
   buildDestinationItems() {
     return Container(
       height: 300,
-      color: Colors.green,
       child: ListView.builder(
         itemBuilder: (context, index) {
           Destination destination = destinationList[index];
           return Container(
-            margin: EdgeInsets.all(16),
-            color: Colors.white,
-            width: 210,
+            margin: EdgeInsets.all(8),
+            padding: EdgeInsets.all(8),
+            height: 150,
+            width: 250,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
             child: Stack(
+              alignment: Alignment.topCenter,
               children: [
-                Container(
-                  margin: EdgeInsets.all(8),
-                  color: Colors.blue,
-                  width: 210,
-                  height: 150,
+                Positioned(
+                  bottom: 5,
+                  child: Container(
+                    margin: EdgeInsets.all(8),
+                    width: 220,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${destination.activityList.length} activities",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                            maxLines: 1,
+                          ),
+                          Text(
+                            destination.description,
+                            style: TextStyle(color: Colors.black, fontSize: 18),
+                            maxLines: 2,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6.0,
+                            offset: Offset(0.0, 2.0))
+                      ]),
+
+                  ///contains the image
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image(
+                          image: AssetImage(destination.imageUrl),
+                          height: 180,
+                          width: 180,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 15,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                destination.city,
+                                style: TextStyle(
+                                    fontSize: 24, color: Colors.white),
+                              ),
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  FontAwesomeIcons.locationArrow,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  destination.country,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 24),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           );
